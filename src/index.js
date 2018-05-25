31edb2bd5cf51512d03a31ca300b25637a4ae39b
Ecksi <jimmy.dechant@gmail.com>
2018-05-24 18:24:47 -0600

    Update index.js for firebase

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
 import rootReducer from './reducers';
 import './index.css';
 import App from './components/App/App';
@@ -14,9 +13,7 @@ const store = createStore(rootReducer, devTools);
 
 const app = (
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </Provider>
 );
 
