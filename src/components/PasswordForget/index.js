38bf07e74b7a422446a9b0b2d0dbf2b734c3f545
Ecksi <jimmy.dechant@gmail.com>
2018-05-24 18:26:43 -0600

    Add PasswordForget component

---
+++ src/components/PasswordForget/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PasswordForgetPage = () => {
+  return (
+    <div>
+      <h1>PasswordForget Page</h1>
+    </div>
+  );
+};
+
+export default PasswordForgetPage;
