f82e22b8c7de7b731116769ebbfced303397fd51
Ecksi <jimmy.dechant@gmail.com>
2018-05-26 14:15:22 -0600

    Setup firebase for react-redux in Navigation component

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,21 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-
+import SignOutButton from '../SignOut';
 import * as routes from '../../constants/routes';
 
-const Navigation = () => {
-  return (
-    <div>
-      <ul>
-        <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
-        <li><Link to={routes.SIGN_UP}>Sign Up</Link></li>
-        <li><Link to={routes.LANDING}>Landing</Link></li>
-        <li><Link to={routes.HOME}>Home</Link></li>
-        <li><Link to={routes.ACCOUNT}>Account</Link></li>
-        <li><Link to={routes.PASSWORD_FORGET}>PASSWORD_FORGET</Link></li>
-      </ul>
-    </div>
-  );
-};
+const Navigation = ({ authUser }) =>
+  <div>
+    {authUser
+      ? <NavigationAuth />
+      : <NavigationNonAuth />
+    }
+  </div>;
 
-export default Navigation;
+const NavigationAuth = () =>
+  <ul>
+    <li><Link to={routes.LANDING}>Landing</Link></li>
+    <li><Link to={routes.HOME}>Home</Link></li>
+    <li><Link to={routes.ACCOUNT}>Account</Link></li>
+    <li><SignOutButton /></li>
+  </ul>;
+
+const NavigationNonAuth = () =>
+  <ul>
+    <li><Link to={routes.LANDING}>Landing</Link></li>
+    <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
+  </ul>;
+
+const mapStateToProps = (state) => ({
+  authUser: state.sessionState.authUser,
+});
+
+export default connect(mapStateToProps)(Navigation);
