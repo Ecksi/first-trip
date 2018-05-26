3c63659be5a94a3cf05ce5585c0ceb373cf804c0
Ecksi <jimmy.dechant@gmail.com>
2018-05-26 14:17:58 -0600

    Setup store for firebase

---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux';
+import rootReducer from '../reducers';
+
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, devTools);
+
+export default store;
