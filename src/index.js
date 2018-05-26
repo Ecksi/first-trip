44a77f1e38c92c9ea51ad993f4a57945ced45814
Ecksi <jimmy.dechant@gmail.com>
2018-05-26 14:20:32 -0600

    Import store for firebase-redux setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './reducers';
+import store from './store';
 import './index.css';
 import App from './components/App/App';
-
-const devTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-const store = createStore(rootReducer, devTools);
 
 const app = (
   <Provider store={store}>
