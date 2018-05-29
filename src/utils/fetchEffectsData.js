9c600503d1195b68ab223dba6e5dc629089271b8
Ecksi <jimmy.dechant@gmail.com>
2018-05-29 07:00:29 -0600

    Add fetch call for strain effects

---
+++ src/utils/fetchEffectsData.js
@@ -0,0 +1,23 @@
+import { strainKey } from '../private/keys';
+import { doFetch } from './doFetch';
+
+const fetchFavoriteData = async () => {
+  const url = `http://strainapi.evanbusse.com/${strainKey}/searchdata/effects`;
+  const effectsData = await doFetch(url);
+
+  return sortedEffects(effectsData);
+};
+
+const sortedEffects = dirtyData => {
+  return dirtyData.reduce((sortedData, data) => {
+    sortedData[data.type].push(data.effect);
+
+    return sortedData;
+  }, {
+    positive: [],
+    medical: [],
+    negative: [],
+  });
+};
+
+export default fetchFavoriteData;
