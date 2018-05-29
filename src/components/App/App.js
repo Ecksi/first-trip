28ca4bffd55d4de88f1649596fff755f9d2f3784
Ecksi <jimmy.dechant@gmail.com>
2018-05-28 21:58:18 -0600

    Remove Navigation component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,6 @@ import {
   BrowserRouter as Router,
   Route,
 } from 'react-router-dom';
-import Navigation from '../Navigation';
 import LandingPage from '../Landing';
 import SignUpPage from '../SignUp';
 import SignInPage from '../SignIn';
@@ -13,13 +12,9 @@ import AccountPage from '../Account';
 import * as routes from '../../constants/routes';
 import withAuthentication from '../../auth/withAuthentication';
 
-const App = () =>
+export const App = () => 
   <Router>
     <div>
-      <Navigation />
-
-      <hr />
-
       <Route exact path={routes.LANDING} component={() => <LandingPage />} />
       <Route exact path={routes.SIGN_UP} component={() => <SignUpPage />} />
       <Route exact path={routes.SIGN_IN} component={() => <SignInPage />} />
