4fee8326360f3d5bfeecff898e70135830e693e1
Ecksi <jimmy.dechant@gmail.com>
2018-05-28 21:53:07 -0600

    Add effects and filters reducers to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,15 @@ import { combineReducers } from 'redux';
 import sessionReducer from './session';
 import userReducer from './user';
 import strainsReducer from './strains';
+import effectsReducer from './effects';
+import filtersReducer from './filters';
 
 const rootReducer = combineReducers({
   sessionState: sessionReducer,
   userState: userReducer,
   strains: strainsReducer,
+  effects: effectsReducer,
+  filters: filtersReducer,
 });
 
 export default rootReducer;
