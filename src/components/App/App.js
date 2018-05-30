915845d44ff58da82ef4fb96c574849e5e7c3234
Ecksi <jimmy.dechant@gmail.com>
2018-05-30 08:31:47 -0600

    Add Results route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,15 @@ import {
   BrowserRouter as Router,
   Route,
 } from 'react-router-dom';
+import withAuthentication from '../../auth/withAuthentication';
+import * as routes from '../../constants/routes';
 import LandingPage from '../Landing';
 import SignUpPage from '../SignUp';
 import SignInPage from '../SignIn';
 import PasswordForgetPage from '../PasswordForget';
 import HomePage from '../Home';
 import AccountPage from '../Account';
-import * as routes from '../../constants/routes';
-import withAuthentication from '../../auth/withAuthentication';
+import ResultContainer from  '../ResultContainer';
 
 export const App = () => 
   <Router>
@@ -21,6 +22,7 @@ export const App = () =>
       <Route exact path={routes.PASSWORD_FORGET} component={() => <PasswordForgetPage />} />
       <Route exact path={routes.HOME} component={() => <HomePage />} />
       <Route exact path={routes.ACCOUNT} component={() => <AccountPage />} />
+      <Route exact path={routes.RESULTS} component={() => <ResultContainer />} />
     </div>
   </Router>;
 
