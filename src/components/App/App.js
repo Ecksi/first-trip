db05e549aba1b49fc9cb42ee5f0f546140a3b6d9
Ecksi <jimmy.dechant@gmail.com>
2018-05-30 15:22:23 -0600

    Update formatting on App to fix linter errors

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,13 +16,34 @@ import ResultContainer from  '../ResultContainer';
 export const App = () => 
   <Router>
     <div>
-      <Route exact path={routes.LANDING} component={() => <LandingPage />} />
-      <Route exact path={routes.SIGN_UP} component={() => <SignUpPage />} />
-      <Route exact path={routes.SIGN_IN} component={() => <SignInPage />} />
-      <Route exact path={routes.PASSWORD_FORGET} component={() => <PasswordForgetPage />} />
-      <Route exact path={routes.HOME} component={() => <HomePage />} />
-      <Route exact path={routes.ACCOUNT} component={() => <AccountPage />} />
-      <Route exact path={routes.RESULTS} component={() => <ResultContainer />} />
+      <Route
+        exact path={routes.LANDING}
+        component={() => <LandingPage />}
+      />
+      <Route
+        exact path={routes.SIGN_UP}
+        component={() => <SignUpPage />}
+      />
+      <Route
+        exact path={routes.SIGN_IN}
+        component={() => <SignInPage />}
+      />
+      <Route 
+        exact path={routes.PASSWORD_FORGET} 
+        component={() => <PasswordForgetPage />} 
+      />
+      <Route
+        exact path={routes.HOME}
+        component={() => <HomePage />}
+      />
+      <Route
+        exact path={routes.ACCOUNT}
+        component={() => <AccountPage />}
+      />
+      <Route
+        exact path={routes.RESULTS}
+        component={() => <ResultContainer />}
+      />
     </div>
   </Router>;
 
