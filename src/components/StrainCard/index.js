5d6cacc923a1c134e2b94fa0f99003e0d319d9bf
Ecksi <jimmy.dechant@gmail.com>
2018-05-30 08:44:00 -0600

    Update strainCard to proplery display all effects and flavors

--- src/components/StrainCard/index.js
+++ src/components/StrainCard/index.js
@@ -1,26 +1,42 @@
 import React from 'react';
+import './StrainCard.css';
 
 const StrainCard = props => {
-  const {
-    name,
-    race,
-    positive,
-    medical,
-    negative,
-    flavors,
-    description,
-  } = props;
-  
+  const { id, name, race, effects, flavors } = props;
+
+  const getEffects = (type, props) => {
+    return effects[type].map(effect => (
+      <li>{effect}</li>
+    ));
+  };
+
+  const getFlavors = props => {
+    return flavors.map(flavor => (
+      <li>{flavor}</li>
+    ));
+  };
+
   return (
-    <div>
+    <div className="strain-card">
       <h1>{name}</h1>
-      <h3>{race}</h3>
+      <h3>Type: {race}</h3>
       <h3>Effects:</h3>
-      <p>Positive: {positive}</p>
-      <p>Medical: {medical}</p>
-      <p>Negative: {negative}</p>
-      <h3>{flavors}</h3>
-      <p>Description: {description}</p>
+      <div className="strain-effects">
+        <div>
+          <h4>Positive</h4>
+          <ul>{getEffects('positive')}</ul>
+        </div>
+        <div>
+          <h4>Medical</h4>
+          <ul>{getEffects('medical')}</ul>
+        </div>
+        <div>
+          <h4>Negative</h4>
+          <ul>{getEffects('negative')}</ul>
+        </div>
+      </div>
+      <h3>Flavors:</h3>
+      <ul className="flavors">{getFlavors()}</ul>
     </div>
   );
 };
