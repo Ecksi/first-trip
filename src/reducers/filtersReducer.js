5345d93c74ef2c3031b382544756c04ef78d2bfb
Ecksi <jimmy.dechant@gmail.com>
2018-06-01 19:48:38 -0600

    Add filters reducer

--- src/reducers/filtersReducer.js
+++ src/reducers/filtersReducer.js
@@ -5,8 +5,19 @@ function filtersReducer(state = INITIAL_STATE, action) {
     case 'ADD_FILTER': {
       return [...state, action.filters];
     }
+    case 'RESET_FILTER': {
+      return [];
+    }
+    case 'REMOVE_FILTER': {
+      const findIndex = state.indexOf(action.filter);
+      const newState = [...state];
+      newState.splice(findIndex, 1);
+
+      return newState;
+    }
     default: return state;
   }
 }
 
 export default filtersReducer;
+// [...state] || state.slice() makes new copy of whole array
