df96344c4c5875a900bcf041a9eabd5865e8d635
Ecksi <jimmy.dechant@gmail.com>
2018-06-01 22:42:45 -0600

    Refactor strainContainer Component to make use of redux

--- src/reducers/searchResultsReducer.js
+++ src/reducers/searchResultsReducer.js
@@ -5,6 +5,30 @@ function searchResultsReducer(state = INITIAL_STATE, action) {
     case 'SEARCH_RESULTS': {
       return [...action.results];
     }
+    case 'SEARCH_BY_FILTERS': {
+      const allResults = action.strains.reduce((scores, strain) => {
+        let score = 0;
+
+        action.filters.forEach(filter =>
+          strain.effects[action.effectType]
+            .includes(filter)
+            ? score++ : null
+        );
+
+        scores[score]
+          ? scores[score].push(strain)
+          : scores[score] = [];
+
+        return scores;
+      }, {});
+
+      const maxMatches = Math.max(...Object.keys(allResults));
+      const minMatches = Math.min(...Object.keys(allResults));
+
+      return action.effectType === 'negative'
+        ? [...state, ...minMatches]
+        : [...state, ...maxMatches];
+    }
     default: return state;
   }
 }
