d34bd4cba96f7edb8b50f0c22682a9958c7fec67
Ecksi <jimmy.dechant@gmail.com>
2018-06-02 18:57:03 -0600

    Add introduction component

---
+++ src/components/Introduction/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Introduction = () => {
+  return (
+    <div>
+      <h3>How and why weed is legal</h3>
+          On November 6, 2012 Amendent 64 passed allowing for the personal use and regulation of marijuana for adults 21 and over.
+    </div>
+  );
+};
+
+export default Introduction;
