f56146c9956cd10fe0c832f3e257c878324f6817
Ecksi <jimmy.dechant@gmail.com>
2018-06-03 15:05:04 -0600

    Remove unnecessary components and containers

--- src/reducers/__tests__/effects.test.js
+++
@@ -1,9 +0,0 @@
-import effectsReducer from '../effects';
-
-describe('effectsReducer', () => {
-  it('should return a default state', () => {
-    let expected = {};
-
-    expect(effectsReducer(undefined, {})).toEqual(expected);
-  });
-});
--- src/reducers/__tests__/session.test.js
+++
@@ -1,9 +0,0 @@
-import sessionReducer from '../session';
-
-describe('sessionReducer', () => {
-  it('returns a default state', () => {
-    let expected = { "authUser": null };
-
-    expect(sessionReducer(undefined, {})).toEqual(expected);
-  });
-});
