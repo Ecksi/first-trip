6117b16b9b6a06d36c04a679c8da2f17e11cb0de
Ecksi <jimmy.dechant@gmail.com>
2018-06-03 15:12:40 -0600

    Add store to root index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 import './index.css';
 import App from './components/App/App';
+
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(rootReducer, devTools);
 
 const app = (
   <Provider store={store}>
