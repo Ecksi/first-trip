9ebd53233589346c05607aa23b1997d5f984dbaa
Ecksi <jimmy.dechant@gmail.com>
2018-06-02 20:37:22 -0600

    Add additional filler text to landing page components

--- src/components/CoLaws/index.js
+++ src/components/CoLaws/index.js
@@ -4,14 +4,26 @@ const CoLaws = () => {
   return (
     <div>
       <h3>Somethings to know</h3>
+      <p>Who can buy? 
+        <span>Anyone over 21</span></p>
+      <p>Where can I consume?
+        <span>You may consume on private property that allows Marijuana consumption. Marijuana may not be consumed openly or pubpcly in Colorado.</span></p>
+      <p>What about edibles?
+        <span>The laws for smoking Marijuana also apply to edibles</span></p>
+      <p>How much can I purchase?
+        <span>Up to one ounce of cannabis flower, 1oz Flower = 8g of Concentrate (Shatter, Wax, etc) 1oz Flower = 800mg of Edibles</span></p>
+      <p>Is it okay to have Marijuana in my car? 
+        <span>Yes, you can carry marijuana in your car, but it must be in a sealed container, and it cannot cross state boundaries. It is illegal to consume marijuana in your car.</span></p>
+      <p>What is the difference between Medical and Retail Marijuana?
+        <span>Only pcensed retail marijuana stores may sell retail marijuana, and only to those 21 and older. Medical marijuana requires a state red card, which can only be obtained by Colorado residents with a recommendation from a doctor.</span></p>
+      <p>How much does it cost?
+        <span>Typically it will cost you around 120 for an ounce.</span></p>
+      <p>For More information visit these resources</p>
       <ul>
-        <li>Who can buy? - Anyone over 21</li>
-        <li>Where can I consume? - You may consume on private property that allows Marijuana consumption. Marijuana may not be consumed openly or publicly in Colorado.</li>
-        <li>What about edibles? - The laws for smoking Marijuana also apply to edibles</li>
-        <li>How much can I purchase? - Up to one ounce of cannabis flower, 1oz Flower = 8g of Concentrate (Shatter, Wax, etc) 1oz Flower = 800mg of Edibles</li>
-        <li>Is it okay to have Marijuana in my car? - Yes, you can carry marijuana in your car, but it must be in a sealed container, and it cannot cross state boundaries. It is illegal to consume marijuana in your car.</li>
-        <li>What is the difference between Medical and Retail Marijuana? - Only licensed retail marijuana stores may sell retail marijuana, and only to those 21 and older. Medical marijuana requires a state red card, which can only be obtained by Colorado residents with a recommendation from a doctor.</li>
-        <li>How much does it cost? - Typically it will cost you around 120 for an ounce.</li>
+        <a href="https://www.colorado.gov/pacific/marijuanainfodenver/residents-visitors">
+          <li>Colorado.gov</li></a>
+        <a href="https://www.coloradopotguide.com/marijuana-laws-in-colorado/">
+          <li>Colorado Pot Guide</li></a>
       </ul>
     </div>
   );
--- src/components/Introduction/index.js
+++ src/components/Introduction/index.js
@@ -4,7 +4,14 @@ const Introduction = () => {
   return (
     <div>
       <h3>How and why weed is legal</h3>
-          On November 6, 2012 Amendent 64 passed allowing for the personal use and regulation of marijuana for adults 21 and over.
+      <span>On November 6, 2012 Amendent 64 passed allowing for the personal use and regulation of marijuana for adults 21 and over.</span>
+      <p>For More information visit these resources</p>
+      <ul>
+        <a href="https://www.colorado.gov/pacific/marijuanainfodenver/residents-visitors">
+          <li>Colorado.gov</li></a>
+        <a href="https://www.coloradopotguide.com/marijuana-laws-in-colorado/">
+          <li>Colorado Pot Guide</li></a>
+      </ul>
     </div>
   );
 };
