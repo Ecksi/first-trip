cc51903c668a5d5f7423377888c2f4591ddd4af0
Ecksi <jimmy.dechant@gmail.com>
2018-06-04 10:18:42 -0600

    Update tests for fetchEffectsData

--- src/utils/__tests__/fetchEffectsData.test.js
+++ src/utils/__tests__/fetchEffectsData.test.js
@@ -1,19 +1,57 @@
 import fetchEffectsData from  '../fetchEffectsData';
-import { doFetch } from '../doFetch';
 
 describe('fetchEffectsData', () => {
   let mockEffectResults;
 
   beforeEach(() => {
-    mockEffectsResults = [
-      {
-        "effect": "Relaxed",
-        "type": "positive"
-      }
-    ];
+    window.fetch = jest.fn().mockImplementation(() => {
+      Promise.resolve({
+        status: 200,
+        ok: true,
+        json: () => Promise.resolve({ effects: {} })
+      });
+    });
   });
 
-  it('calls doFetch', async () => {
-    expect(mockEffectResults).toHaveBeenCalledTimes(1);
+  it('calls fetch with the correct params', async () => {
+    const url = 'http://strainapi.com/effects';
+    await fetchEffectsData(url);
+
+    expect(window.fetch).toHaveBeenCalledTimes(1);
+    expect(window.fetch).toHaveBeenCalledWith(url);
+  });
+
+  it('returns response data if response.ok is true', async() => {
+    await expect(fetchEffectsData()).resolves.toEqual([{ effects: "positive" }]);
+  });
+
+  it('throws an error if response.ok is false', async () => {
+    const mockError = jest.fn();
+    window.fetch = jest.fn().mockImplementation(() => {
+      Promise.resolve({
+        status: 500,
+        ok: false,
+        json: () => Promise.resolve({ effects: 'positive'})
+      });
+
+      expect(window.fetch).toEqual(mockError);
+    });
+  });
+
+  it('throw an error if fetch fails', async () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.reject(Error('mock error')));
+    const expected = ('Error: Network request failed. (error: mock error)');
+    
+    await expect(fetchEffectsData()).rejects.toEqual(expected);
+  });
+
+  describe('sortedEffects', () => {
+    // it('returns sorted effects', () => {
+    //   const mockSortedResults = {
+    //     positive: [],
+    //     medical: [],
+    //     negative: [],
+    //   };
+    // });
   });
 });
