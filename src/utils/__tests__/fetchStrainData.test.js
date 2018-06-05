d265604825a0ab0ab8e099d0fa03cd29e503c346
Ecksi <jimmy.dechant@gmail.com>
2018-06-05 08:08:53 -0600

    Update fetchStrainData tests to have 100% coverage

--- src/utils/__tests__/fetchStrainData.test.js
+++ src/utils/__tests__/fetchStrainData.test.js
@@ -1,61 +1,71 @@
-import fetchStrainData from '../fetchStrainData';
+import fetchStrainData, { cleanStrainData } from '../fetchStrainData';
+import { mockStrains } from '../__mocks__/fetchStrainData';
+
+jest.mock('../../private/keys', () => () => undefined);
 
 describe('fetchStrainData', () => {
-  let mockStrainResults;
-
   beforeEach(() => {
-    window.fetch = jest.fn().mockImplementation(() => 
-      Promise.resolve({
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
         status: 200,
         ok: true,
-        json: () => Promise.resolve({ strainData: {} })
-      }));
+        json: () => Promise.resolve({})
+      });
+    });
   });
 
   it('calls fetch with the correct params', async () => {
-    const url = 'http://strainapi.com';
+    const url = 'http://strainapi.evanbusse.com/undefined/strains/search/all';
+
+    expect(window.fetch).not.toHaveBeenCalled();
     await fetchStrainData(url);
     
     expect(window.fetch).toHaveBeenCalledTimes(1);
     expect(window.fetch).toHaveBeenCalledWith(url);
   });
 
-  it('returns response data if response.ok is true', async () => {
-    await expect(fetchStrainData()).resolves.toEqual([{ "name": "strainData" }]);
+  it('returns response data if response.ok is true', () => {
+    expect(fetchStrainData()).resolves.toEqual([]);
   });
 
-  it('throws an error if response.ok is false', async () => {
-    const mockError = jest.fn();
+  it('handles error if response.ok is false', async () => {
+    const mockError = new Error('Network request failed. (error: 500)');
     window.fetch = jest.fn().mockImplementation(() => {
-      Promise.resolve({
+      return Promise.resolve({
         status: 500,
         ok: false,
-        json: () => Promise.resolve({ strainData: 'why tho'})
       });
     });
 
-    expect(window.fetch).toEqual(mockError);
+    expect(fetchStrainData()).rejects.toEqual(mockError);
   });
 
   it('throws an error if fetch fails', async () => {
     window.fetch = jest.fn().mockImplementation(() => Promise.reject(Error('mock error')));
-    const expected = ('Error: Network request failed. (error: mock error)');
+    const expected = new Error('Network request failed. (error: mock error)');
 
     await expect(fetchStrainData()).rejects.toEqual(expected);
   });
 });
 
 describe('cleanStrainData', () => {
-  // it('returns clean strain data', () => {
-  //   const mockCleanStrainData = [
-  //     {
-  //       id: 1,
-  //       race: 'hybrid',
-  //       flavors: [],
-  //       effects: {},
-  //       name: 'bluesky',
-  //     }
-  //   ];
-  // });
+  it('returns cleanStrainData', () => {
+    const expected = [
+      {
+        id: 1,
+        race: 'hybrid',
+        flavors: ["Earthy", "Chemical", "Pine"],
+        effects: { 
+          "medical": ["Depression", "Insomnia", "Pain", "Stress", "Lack of Appetite"],
+          "negative": ["Dizzy"],
+          "positive": ["Relaxed", "Hungry", "Happy", "Sleepy"] },
+        name: 'Afpak',
+      }
+    ];
+
+    const result = cleanStrainData(mockStrains);
+
+    expect(result).toEqual(expected);
+  });
 });
 
