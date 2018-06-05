c0d8d335b3ca16202fa8dbdb39a6f88929b3e208
Ecksi <jimmy.dechant@gmail.com>
2018-06-05 17:56:16 -0600

    Update styling for CoLaws component

--- src/components/CoLaws/index.js
+++ src/components/CoLaws/index.js
@@ -6,17 +6,17 @@ const CoLaws = () => {
     <div className='co-laws'>
       <h1 className='header'>Somethings to know</h1>
       <p>Who can buy? 
-        <span>Anyone over 21</span></p>
+        <span>Anyone over 21</span></p><br />
       <p>Where can I consume?
-        <span>You may consume on private property that allows Marijuana consumption. Marijuana may not be consumed openly or pubpcly in Colorado.</span></p>
+        <span>You may consume on private property that allows Marijuana consumption. Marijuana may not be consumed openly or pubpcly in Colorado.</span></p><br />
       <p>What about edibles?
-        <span>The laws for smoking Marijuana also apply to edibles</span></p>
+        <span>The laws for smoking Marijuana also apply to edibles</span></p><br />
       <p>How much can I purchase?
-        <span>Up to one ounce of cannabis flower, 1oz Flower = 8g of Concentrate (Shatter, Wax, etc) 1oz Flower = 800mg of Edibles</span></p>
+        <span>Up to one ounce of cannabis flower, 1oz Flower = 8g of Concentrate (Shatter, Wax, etc) 1oz Flower = 800mg of Edibles</span></p><br />
       <p>Is it okay to have Marijuana in my car? 
-        <span>Yes, you can carry marijuana in your car, but it must be in a sealed container, and it cannot cross state boundaries. It is illegal to consume marijuana in your car.</span></p>
+        <span>Yes, you can carry marijuana in your car, but it must be in a sealed container, and it cannot cross state boundaries. It is illegal to consume marijuana in your car.</span></p><br />
       <p>What is the difference between Medical and Retail Marijuana?
-        <span>Only pcensed retail marijuana stores may sell retail marijuana, and only to those 21 and older. Medical marijuana requires a state red card, which can only be obtained by Colorado residents with a recommendation from a doctor.</span></p>
+        <span>Only pcensed retail marijuana stores may sell retail marijuana, and only to those 21 and older. Medical marijuana requires a state red card, which can only be obtained by Colorado residents with a recommendation from a doctor.</span></p><br />
       <p>How much does it cost?
         <span>Typically it will cost you around 120 for an ounce.</span></p>
       <br />
