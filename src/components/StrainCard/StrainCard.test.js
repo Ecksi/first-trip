df4b65a76fb70c1549a2e020d7944f4ee20f3ff8
Ecksi <jimmy.dechant@gmail.com>
2018-06-05 15:04:06 -0600

    Update StrainCard test

--- src/components/StrainCard/StrainCard.test.js
+++ src/components/StrainCard/StrainCard.test.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import StrainCard from './index';
+import { StrainCard } from './index';
 
 describe('StrainCard', () => {
   let strainCard;
+  let mockProps = {
+    name: 'yoyiyiyo',
+    race: 'aintants',
+    effects: [1, 2, 3],
+    flavors: [1, 2, 3],
+  };
 
   beforeEach(() => {
-    strainCard = shallow(<StrainCard />);
+    strainCard = shallow(<StrainCard {...mockProps} />);
   });
 
   it('should match the snapshot', () => {
